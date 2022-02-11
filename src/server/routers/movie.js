1d1c11a266a2f67e44073294323446c22247e76a
Nathan King <nathanking3590@gmail.com>
2022-02-11 17:25:15 +0000

    Remove boilerplate

--- src/server/routers/movie.js
+++ src/server/routers/movie.js
@@ -1,12 +1,8 @@
 const express = require('express');
-const {
-    getAllMovies,
-    createMovie
-} = require('../controllers/movie');
+
 
 const router = express.Router();
 
-router.get('/', getAllMovies);
-router.post('/', createMovie);
+
 
 module.exports = router;
